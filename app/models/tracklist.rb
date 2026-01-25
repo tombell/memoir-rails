6ac54dc40f9f978fa8739dde79696099865eab6a
Tom Bell <tomb@tombell.dev>
2026-01-25 23:55:40 +0000

    Add ransackable attrs to tracklist model

--- app/models/tracklist.rb
+++ app/models/tracklist.rb
@@ -1,4 +1,8 @@
 class Tracklist < ApplicationRecord
   has_many :tracklists_tracks, dependent: :destroy
   has_many :tracks, through: :tracklists_tracks
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name date]
+  end
 end
