661c37f4a307eed853c0c2a595bfbe774e895306
Tom Bell <tomb@tombell.dev>
2026-01-26 01:49:29 +0000

    Add validations to models

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,6 @@
 class Track < ApplicationRecord
   has_many :tracklists_tracks, dependent: :destroy
   has_many :tracklists, through: :tracklists_tracks
+
+  validates :artist, :name, :genre, :bpm, :key, presence: true
 end
