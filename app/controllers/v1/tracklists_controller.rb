3b0a87c23c0746c4561bddb9152a7eb397b398dd
Tom Bell <tomb@tombell.dev>
2026-01-26 00:12:27 +0000

    Add filtering and sorting to tracklists index

--- app/controllers/v1/tracklists_controller.rb
+++ app/controllers/v1/tracklists_controller.rb
@@ -1,8 +1,12 @@
 module V1
   class TracklistsController < ApiController
+    FILTERABLE_FIELDS = %i[name date]
+
     def index
-      jsonapi_paginate(Tracklist.all) do |paginated|
-        render jsonapi: paginated
+      jsonapi_filter(Tracklist.all, FILTERABLE_FIELDS) do |filtered|
+        jsonapi_paginate(filtered.result) do |paginated|
+          render jsonapi: paginated
+        end
       end
     end
 
