5e56c3f2e9e0e1e2c820c4e4e44a23f58acea047
Tom Bell <tomb@tombell.dev>
2026-01-26 01:49:29 +0000

    Add validations to models

--- app/models/tracklist.rb
+++ app/models/tracklist.rb
@@ -2,6 +2,8 @@ class Tracklist < ApplicationRecord
   has_many :tracklists_tracks, dependent: :destroy
   has_many :tracks, through: :tracklists_tracks
 
+  validates :name, :date, :artwork, :url, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     %w[name date]
   end
