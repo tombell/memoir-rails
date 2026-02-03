f10181f97bbd3969f6fa1b60ad788c7206849426
Tom Bell <tomb@tombell.dev>
2026-02-03 16:04:23 +0000

    Add JSONAPI::Deserialization to api controller

--- app/controllers/v1/api_controller.rb
+++ app/controllers/v1/api_controller.rb
@@ -1,6 +1,7 @@
 module V1
   class ApiController < ApplicationController
     include JSONAPI::Errors
+    include JSONAPI::Deserialization
     include JSONAPI::Fetching
     include JSONAPI::Filtering
     include JSONAPI::Pagination
