0f8a74efc112bb7f0574c506c922754c9de5071a
Tom Bell <tomb@tombell.dev>
2026-03-04 23:31:06 +0000

    Update tracklists and tracks controllers

--- app/controllers/v1/tracklists_controller.rb
+++ app/controllers/v1/tracklists_controller.rb
@@ -8,6 +8,36 @@ module V1
 
     def show
       render jsonapi: Tracklist.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render jsonapi_errors: [ { title: "Not Found", detail: e.message } ], status: :not_found
+    end
+
+    def create
+      tracklist = Tracklists::CreateService.call(tracklist: tracklist_params)
+
+      render jsonapi: tracklist, status: :created
+    rescue ActiveRecord::RecordInvalid => e
+      render jsonapi_errors: e.record.errors, status: :unprocessable_entity
+    end
+
+    def update
+      tracklist = Tracklists::UpdateService.call(id: params[:id], tracklist: tracklist_params)
+      render jsonapi: tracklist
+    rescue ActiveRecord::RecordInvalid => e
+      render jsonapi_errors: e.record.errors, status: :unprocessable_entity
+    end
+
+    def destroy
+      Tracklists::DeleteService.call(id: params[:id])
+      head :no_content
+    rescue ActiveRecord::RecordNotFound => e
+      render jsonapi_errors: [ { title: "Not Found", detail: e.message } ], status: :not_found
+    end
+
+    private
+
+    def tracklist_params
+      jsonapi_deserialize(params, only: %i[name date artwork url])
     end
   end
 end
